77be420b38215f4f90e0e04c6ae0ce8a62d97bb6
Marty Fitchen and Sam Park <marty+sam@>
2016-08-29 11:10:25 -0700

    Add admin and recipe routes and components and starting now on new recipe

---
+++ app/models/recipe.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr(),
+  cuisine: DS.attr(),
+  dish: DS.attr(),
+  time: DS.attr(),
+  serves: DS.attr(),
+  ingredients: DS.attr(), //HAS MANY?
+  instructions: DS.attr() //HAS MANY?
+
+});
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('recipe');
+  },
+  actions: {
+    save(params) {
+      var newRecipe = this.store.createRecord('recipe', params);
+      newRecipe.save();
+      this.transitionTo('admin');
+    },
+
+    update(recipe, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          recipe.set(key,params[key]);
+        }
+      });
+      recipe.save();
+      this.transitionTo('admin');
+    },
+
+    destroyRecipe(recipe) {
+      recipe.destroyRecord();
+      this.transitionTo('admin');
+    }
+  }
 });
