e0d0c7a31776a2e6e104df93591177562ef17db5
Alvin Ng and Tony Fabery <alvin+tony@>
2016-08-30 11:22:04 -0700

    begin to implement supermarket API

--- app/routes/recipe.js
+++ app/routes/recipe.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-     var key = config.myApiKey;
-     var url = 'http://www.supermarketapi.com/api.asmx/COMMERCIAL_SearchByProductName?APIKEY=' +key+ '&ItemName=' + params;
-     return Ember.$.getJSON(url).then(function(responseJSON) {
-       return responseJSON.results;
-     });
+   var key = config.myApiKey;
+   var url = 'http://www.supermarketapi.com/api.asmx/COMMERCIAL_SearchByProductName?APIKEY=' +key+ '&ItemName=' + params;
+   return Ember.$.getJSON(url).then(function(responseJSON) {
+     return responseJSON.results;
+   });
   }
 });
