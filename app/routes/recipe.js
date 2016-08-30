8ef5626b5156592d6c00f7bb5bff3b3b1dbc3bdd
Alvin Ng and Tony Fabery <alvin+tony@>
2016-08-30 11:07:32 -0700

    add basic api structure

--- app/routes/recipe.js
+++ app/routes/recipe.js
@@ -1,5 +1,12 @@
+import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
-});
+  model: function(params) {
+     var key = config.myApiKey;
+     var url = 'http://www.supermarketapi.com/api.asmx/COMMERCIAL_SearchByProductName?APIKEY=' +key+ '&ItemName=' + params;
+     return Ember.$.getJSON(url).then(function(responseJSON) {
+       return responseJSON.results;
+     });
+   }
+ });
--- app/routes/result.js
+++ app/routes/result.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model: function(params) {
-    console.log(params.cuisine);
     return this.store.query('recipe', {
       orderBy: 'cuisine' || 'dish',
       equalTo: params.cuisine.split(', ')[0] || params.cuisine.split(', ')[1],
