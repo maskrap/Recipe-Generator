3234bf09016ac6f44d5cc94e03c06df2881feab4
Alvin Ng and Tony Fabery <alvin+tony@>
2016-08-31 13:35:43 -0700

    reconfigure the filtering of the generated recipes

--- app/routes/result.js
+++ app/routes/result.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   favoritesList: Ember.inject.service(),
-  temperature: '',
-
   model: function(params) {
-    this.set('temperature', params.cuisine.split(', ')[1]);
-    return this.store.query('recipe', {
-      orderBy: 'cuisine',
-      equalTo: params.cuisine.split(', ')[0]
+    return Ember.RSVP.hash({
+      recipes: this.store.query('recipe', {
+        orderBy: 'cuisine',
+        equalTo: params.cuisine.split(', ')[0]
+      }),
+      temperature: params.cuisine.split(', ')[1]
     });
   }
 });
