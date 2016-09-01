609bc21c23a1ebe34890a162af15f910aebb90d6
Tony Fabery <tfabery@gmail.com>
2016-09-01 10:16:17 -0700

    add if statement to handle empty ingredient data

--- app/components/ingredient-list.js
+++ app/components/ingredient-list.js
@@ -5,18 +5,21 @@ export default Ember.Component.extend({
   ingredientResult: Ember.computed('ingredient', function() {
     var key = config.myApiKey;
     var that = this;
-    var url = 'http://www.supermarketapi.com/api.asmx/COMMERCIAL_SearchByProductName?APIKEY=' + key + '&ItemName=' + this.get('ingredient').toLowerCase();
-    return Ember.$.get(url).then(function(responseJSON) {
-      var prices = responseJSON.getElementsByTagName("Pricing");
-      var priceArray = [];
-      for (var i = 0; i < prices.length; i ++) {
-        var price = prices[i].childNodes[0].nodeValue;
-        priceArray.push(price);
-      }
-      priceArray.sort(function(a, b) {
-        return a-b;
-      });
-      that.set('ingredientResult', parseFloat(priceArray[0]).toFixed(2));
-    });
+    var ingredient = this.get('ingredient').toLowerCase()
+    var url = 'http://www.supermarketapi.com/api.asmx/COMMERCIAL_SearchByProductName?APIKEY=' + key + '&ItemName=' + ingredient;
+    if (ingredient) {
+      return Ember.$.get(url).then(function(responseJSON) {
+        var prices = responseJSON.getElementsByTagName("Pricing");
+        var priceArray = [];
+        for (var i = 0; i < prices.length; i ++) {
+          var price = prices[i].childNodes[0].nodeValue;
+          priceArray.push(price);
+        }
+        priceArray.sort(function(a, b) {
+          return a-b;
+        });
+        that.set('ingredientResult', parseFloat(priceArray[0]).toFixed(2));
+      });  
+    }
   })
 });
