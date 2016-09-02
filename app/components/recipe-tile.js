50d6c9ff87b6b899a6c51eea763d7a831fd49f9c
Marty Fitchen <mfitchen@gmail.com>
2016-09-02 08:25:20 -0700

    merged styles and add working favorites list

--- app/components/recipe-tile.js
+++ app/components/recipe-tile.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     addToFav(recipe) {
-      this.sendAction('addToFav', recpie);
+      this.sendAction('addToFav', recipe);
     },
   }
 });
