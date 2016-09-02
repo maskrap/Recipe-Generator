98058c6d21ed21b4715f81ab56a43eb016459e52
Tony Fabery <tfabery@gmail.com>
2016-09-01 20:44:01 -0700

    add wine pairing for cod dishes

--- app/components/wine-pairing.js
+++ app/components/wine-pairing.js
@@ -3,18 +3,28 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   dishResult: Ember.computed('recipe', function() {
-    console.log(this.get('recipe'));
-    var that = this;
     var key = config.myApiKey2;
-    var url = 'http://services.wine.com/api/beta2/service.svc/JSON/catalog?filter=type&apikey=' + 'cc9b6e601c6fe9cde1aeab629aa12119';
+    var that = this;
+    var urlRed = 'http://services.wine.com/api/beta2/service.svc/JSON/catalog?filter=catagories(490+124)&apikey=' + key;
+    var urlWhite = 'http://services.wine.com/api/beta2/service.svc/JSON/catalog?filter=catagories(490+125)&apikey=' + key;
     if (this.get('recipe').get('dish').toLowerCase() === 'hot') {
-      return Ember.$.getJSON(url).then(function(responseJSON) {
+      return Ember.$.getJSON(urlRed).then(function(responseJSON) {
         var wines = responseJSON.Products.List;
         var wineArray = [];
         wines.forEach(function(wine) {
           wineArray.push(wine.Name);
         });
-        console.log(wineArray[Math.floor(Math.random()*wineArray.length)]);
+        var rndWine = wineArray[Math.floor(Math.random()*wineArray.length)];
+        that.set('dishResult', rndWine);
+      });
+    }
+    else {
+      return Ember.$.getJSON(urlWhite).then(function(responseJSON) {
+        var wines = responseJSON.Products.List;
+        var wineArray = [];
+        wines.forEach(function(wine) {
+          wineArray.push(wine.Name);
+        });
         var rndWine = wineArray[Math.floor(Math.random()*wineArray.length)];
         that.set('dishResult', rndWine);
       });
